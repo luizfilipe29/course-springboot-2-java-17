17015363bb0d85ab668acdda86e3409d0cdd5419
luizfilipe29 <luiz_filipe.29@hotmail.com>
2022-02-07 19:09:14 -0300

    JPA repository, injecao de dependencia, e populando o database

---
+++ src/main/java/com/luizfilipe/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.luizfilipe.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luizfilipe.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
